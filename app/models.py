c89e3d3220d97ef0180468f70f65a3255ed79605
devszymon <szymon070506@gmail.com>
2024-02-23 00:04:21 +0100

    initial commit, defined models

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Task(models.Model):
+    title = models.TextField,
+    description = models.TextField,
+    completed = models.BooleanField,
+    category = models.ForeignKey(
+        Category,
+        related_name='tasks',
+        on_delete=models.CASCADE
+    )
