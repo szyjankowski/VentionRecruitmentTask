b0798696977ec7fc12a9eed8fcae323452ae370c
devszymon <szymon070506@gmail.com>
2024-02-23 00:49:42 +0100

    feat: admin pages for models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from app.models import Category, Task
 
 # Register your models here.
+
+admin.site.register(Category)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "completed", "category")
